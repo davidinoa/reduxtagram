e30eba5a5aee47231b822a14395a6e76cd8a14b8
David Inoa <davidinoa@gmail.com>
2018-03-02 15:20:39 -0800

    Create Single and PhotoGrid components

---
+++ client/components/Single.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Single extends Component {
+
+  render () {
+    return (
+      <div className="single-photo">
+        I'm the Single
+      </div>
+    );
+  }
+
+}
+
+export default Single;
